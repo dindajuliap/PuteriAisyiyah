837a7cdd174ae1cffecbaa1812d294828e778add
fildzahalifia <fildzah.alifia01@gmail.com>
2021-06-02 11:51:01 +0700

    oalah

--- application/views/Templates/foot.php
+++ application/views/Templates/foot.php
@@ -4,7 +4,7 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.0/dist/sweetalert2.js"></script>
 	<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
-	<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+	<!-- <script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script> -->
 	<script src="<?= base_url('assets/plugins/pace-progress/pace.min.js') ?>"></script>
   <script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
   <script src="<?= base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
