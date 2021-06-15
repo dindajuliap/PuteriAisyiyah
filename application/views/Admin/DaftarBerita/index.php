ec8da89a9552d945fece50449ed858d398a2fb9d
fildzahalifia <fildzah.alifia01@gmail.com>
2021-06-15 18:16:59 +0700

    tabel petugas, donasi, berita

--- application/views/Admin/DaftarBerita/index.php
+++ application/views/Admin/DaftarBerita/index.php
@@ -1,56 +1,102 @@
-<body style="font-family: Arial">
-  <div class="container post mt-4 mb-4">
-    <div class="card">
-      <h2 style="color: #030153; font-weight: 700" align="center" class="mt-4">DAFTAR BERITA</h2>
+<body>
+	<div class="container-fluid" style="padding: 3%; margin-top: -3%;">
+		<div class="row">
+			<div class="col-12">
+				<div class="card" style="padding: 2%;">
+					<h1 class="mt-1" style="text-align: center; color: #030153;">Daftar Berita</h1>
+					<?= $this->session->flashdata('message') ?>
 
-      <div class="row mt-3 mb-3" style="margin-left: auto; margin-right: auto">
-        <div class="col-lg-3">
-          <a href="<?= base_url('Admin/DaftarBerita/TambahDataBerita') ?>">
-            <button class="btn" style="width: 170px; height: 40px; background-color: #030153; color: white; margin-left: 17%">
-              <i class="fas fa-plus-circle mr-2"></i>
-              Tambah Data
-            </button>
-          </a>
-        </div>
+					<div class="col" style="margin-left: -16%;">
+						<a href="<?= base_url('Admin/DaftarBerita/TambahDataBerita') ?>">
+							<button class="btn" style="width: 170px; height: 40px; background-color: #030153; color: white; margin-left: 17%">
+								<i class="fas fa-plus-circle mr-2"></i>
+								Tambah Data
+							</button>
+						</a>
+					</div>
+					
+					<?php if(!$berita) : ?>
+						<div class="card-body">
+							<table id="example1" class="table table-bordered table-striped">
+								<thead style="background-color: #CAA615;">
+									<tr style="text-align: center">
+										<th>No.</th>
+										<th>Judul</th>
+										<th>Foto</th>
+										<th>Pengaturan</th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+					<?php else : ?>
+						<div class="card-body">
+							<table id="example1" class="table table-bordered table-striped">
+								<thead style="background-color: #CAA615;">
+									<tr style="text-align: center">
+										<th>No.</th>
+										<th>Judul</th>
+										<th>Foto</th>
+										<th>Pengaturan</th>
+									</tr>
+								</thead>
 
-        <div class="col-lg-9">
-          <i class="fa fa-search icon" style="margin-left: 62%; margin-top: 1%; color: #AFAFAF"></i>
-          <input type="text" placeholder="Search" name="search_text" id="search_text" class="form-control" style="border-radius: 20px; padding: 20px 40px; width: 35%; margin-left: 60%; margin-top: -4.1%">
-        </div>
-      </div>
+								<tbody>
+									<?php
+										$no = 1;
+										foreach($berita as $val){
+									?>
 
-      <div class="row mt-3 mb-3" style="margin-left: auto; margin-right: auto">
-        <table class="table table-bordered" style="width: 90%" align="center">
-          <tr style="text-align: center; background-color: #CAA615; color: black">
-            <td>No</td>
-            <td>Judul Berita</td>
-            <td>Foto</td>
-            <td>Pengaturan</td>
-          </tr>
+									<tr>
+										<td style="text-align: center"><?= $no ?>.</td>
+										<td><?= $val->judul_berita ?></td>
+										<td style="text-align: center;"><?= $val->cover_berita ?></td>
+										<td style="text-align: center">
+											<a href="<?= site_url('Admin/DetailDataBerita/'.$val->id_berita)?>" class="btn btn-success btn-sm">
+												<i class="fas fa-eye"></i>
+											</a>
+											<a href="<?= site_url('Admin/UbahDataBerita/'.$val->id_berita)?>" class="btn btn-warning btn-sm">
+												<i class="fas fa-edit"></i>
+											</a>
+											<a class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapusModal<?= $val->id_berita ?>" style="color: white" type="submit">
+												<i class="fas fa-trash"></i>
+											</a>
+										</td>
+									</tr>
 
-          <?php if($berita) : ?>
-            <?php $no = 1 ?>
-            <?php foreach ($berita as $data) : ?>
-              <tr>
-                <td style="color: black" align="center"><?= $no++ ?></td>
-                <td style="color: black"><?= $data->judul_berita ?></td>
-                <td>
-                  <img src="<?= base_url() ?>assets/img/foto_berita/<?= $data->cover_berita ?>" style="width: 70%; margin-top: 12.9%; margin-bottom: 12.8%">
-                </td>
-                <td style="color: black" align="center">
-                  <a href="" style="color: black"><i class="fas fa-eye ml-3 fa-lg"></i><a>
-                  <a href="" style="color: green"><i class="fas fa-edit ml-3 fa-lg"></i></a>
-                  <a href="" style="color: red"><i class="fas fa-trash-alt ml-3 fa-lg"></i></a>
-                </td>
-              </tr>
-            <?php endforeach ?>
-          <?php else : ?>
-            <tr>
-              <td colspan="4" align="center">Belum ada berita yang ditambahkan</td>
-            </tr>
-          <?php endif ?>
-        </table>
-      </div>
-    </div>
-  </div>
-</body>
+									<?php $no++; } ?>
+								</tbody>
+							</table>
+						</div>
+					<?php endif ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php foreach($berita as $val) : ?>
+		<div class="modal fade" id="hapusModal<?= $val->id_berita ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content" style="border-radius: 5px">
+					<div class="modal-body">
+						<span>
+							<p style="border-radius: 50%; border: 4px solid #FACEA8; width: 85px; height: 85px; margin-left: auto; margin-right: auto; margin-top: 30px"></p>
+							<p style="color: #F8BB86; font-size: 60px; margin-top: -105px; margin-left: 225px">!</p>
+						</span>
+
+						<h3 class="modal-title" id="exampleModalLabel" align="center">
+							<b style="font-family: Arial; color: #595959">Hapus Data Berita</b>
+						</h3>
+
+						<h5 class="modal-title" id="exampleModalLabel" align="center" style="color: #545454">Anda yakin ingin menghapus data berita ini?</h5>
+
+						<br>
+
+						<div class="row mb-2">
+							<a class="btn" href="<?= base_url('Admin/HapusDataBerita/') . $val->id_berita ?>" style="background: #30454A; color: white; margin-left: auto; margin-right: 10px; width: 105px; padding: 10px">Yakin</a>
+							<button class="btn" type="button" data-dismiss="modal" style="background: grey; color: white; margin-right: auto; margin-left: 10px; width: 105px; padding: 10px">Tidak</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endforeach ?>
