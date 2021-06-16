a93737d15fd831d3d87e3c9c1fcb3f2e34b0580c
fildzahalifia <fildzah.alifia01@gmail.com>
2021-06-17 01:45:13 +0700

    tambah berita

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -500,6 +500,57 @@
       $this->load->view('Templates/foot');
     }
 
+		public function TambahBerita(){
+			$data['judul'] 			= 'Tambah Berita';
+			$data['berita']			= $this->db->get('tabel_berita')->result();
+			$data['tabel_akun']	= $this->db->get_where('tabel_akun', ['email_user' => $this->session->userdata('email_user')])->row_array();
+
+			$this->form_validation->set_rules('judul_berita', 'Judul berita', 'required');
+			$this->form_validation->set_rules('cover_berita', 'Sampul berita', 'required');
+			$this->form_validation->set_rules('isi_berita', 'Isi berita', 'required');
+			$this->form_validation->set_rules('tanggal_berita', 'Tanggal posting', 'required');
+			$this->form_validation->set_message('required', '%s tidak boleh kosong');
+
+			if($this->form_validation->run() == FALSE){
+				$this->load->view('Templates/head', $data);
+				$this->load->view('Templates/navbarAdmin');
+				$this->load->view('Admin/DaftarBerita/TambahBerita', $data);
+				$this->load->view('Templates/foot');
+			}
+			else{
+				$judul_berita		= $this->input->post('judul_berita');
+				$cover_berita		= $this->input->post('cover_berita');
+				$isi_berita			= $this->input->post('isi_berita');
+				$tanggal_berita	= $this->input->post('tanggal_berita');
+
+				$config['upload_path']	 = './assets/img/foto_berita';
+				$config['allowed_types'] = 'gif|jpg|png|jpeg';
+				$config['max_size']      = '2048';
+
+				$this->load->library('upload', $config);
+
+				if (!$this->upload->do_upload('foto_berita')) {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" style="font-family: Arial">&times;</button>Sampul berita harus diisi dengan sesuai</div>');
+					redirect('Admin/TambahBerita');
+				}
+				else {
+					$foto_berita = $this->upload->data();
+					$foto_berita = $foto_berita['file_name'];
+
+					$data = [
+						'judul_berita'		=> $judul_berita,
+						'cover_berita'		=> $cover_berita,
+						'isi_berita'			=> $isi_berita,
+						'isi_berita'			=> $isi_berita
+					];
+					$this->db->insert('tabel_berita', $data);
+
+					$this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" style="font-family: Arial">&times;</button>Berita berhasil ditambahkan</div>');
+					redirect('Admin/DaftarBerita');
+				}
+			}
+		}
+
     public function DaftarInventaris(){
       $data['judul']      = 'Daftar Inventaris';
       $config['base_url'] = 'http://localhost/PuteriAisyiyah/Admin/DaftarInventaris/index';
