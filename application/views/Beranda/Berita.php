0ab8084f177449c411c890bf8129427db3b450b7
dindajuliap <dindajuliap30@gmail.com>
2021-06-19 04:29:18 +0700

    berita lainnya

--- application/views/Beranda/Berita.php
+++ application/views/Beranda/Berita.php
@@ -10,20 +10,24 @@
 		<p style="color: #5E5E5E"><i>Diposting tanggal : <?= date("d F Y", strtotime($berita['tanggal_berita'])) ?></i></p>
 	</div>
 
-	<div style="margin: 10px 100px 10px 100px; width: 100vh;">
+	<div style="margin: 10px 100px 10px 100px">
 		<h5><b>Berita Lainnya</b></h5>
 		<hr style="border: 1px solid #CAA615">
 
 		<div class="row mb-4">
 			<?php foreach ($s_berita as $b) : ?>
 				<div class="col-lg-6 d-md-block mt-3">
-					<div class="card" style="width: 95%; margin-left: auto; margin-right: auto; padding: 20px">
+					<div class="card" style="width: 100%; height: 120px; margin-left: auto; margin-right: auto; padding: 20px">
 						<a href="<?= base_url('Beranda/Berita/') . $b->id_berita ?>" style="color: #030153">
-							<img src="<?= base_url('assets/img/foto_berita/') . $b->cover_berita ?>" style="max-height: 70px; border-radius: 0px">
-
-							<span style="margin: 0px 10px; font-size: 18px">
-								<b><?= $b->judul_berita ?></b>
-							</span>
+							<table valign="middle">
+								<tr>
+									<td width="150px">
+										<div style="height: 70px; position: relative">
+											<img src="<?= base_url('assets/img/foto_berita/') . $b->cover_berita ?>" style="border-radius: 0px; width: 100%; height: 100%; object-fit: cover">
+										</div>
+									</td>
+									<td style="padding: 15px"><b><?= $b->judul_berita ?></b></td>
+							</table>
 						</a>
 					</div>
 				</div>
