7ead08a826eb1785fd0e492bbbd10ff273eff626
meinalisaa <meinalisa02@gmail.com>
2021-06-23 17:18:28 +0700

    cetak data anak (done)

--- application/views/Admin/DaftarPengurus/index.php
+++ application/views/Admin/DaftarPengurus/index.php
@@ -15,6 +15,12 @@
                   Tambah Data
                 </button>
               </a>
+              <a href="<?= base_url('Cetak/CetakDataPengurus') ?>" style="margin-left: -15%">
+                <button class="btn" style="width: 210px; height: 40px; background-color: #030153; color: white; margin-left: 17%">
+                  <i class="fas fa-print mr-2"></i>
+                  Cetak Data Pengurus
+                </button>
+              </a>
             </div>
 
 	          <div class="col-lg-6">
